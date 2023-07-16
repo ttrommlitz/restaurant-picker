7abfcc4f2a3af80df29a0b96d65b416c2c4c0b06
Tyler Trommlitz <ttrommlitz6@gmail.com>
2023-07-15 19:35:02 -0600

    test: returns correct restaurants

---
+++ server/tests/getOpenRestaurants.test.js
@@ -0,0 +1,87 @@
+import assert from 'assert';
+import { getOpenRestaurants } from '../getOpenRestaurants.js';
+
+describe('Get Open Restaurants', function () {
+  it('Returns correct restaurants for Sunday at 12:15 am', function () {
+    const openRestaurants = getOpenRestaurants(7, '12:15 am')
+
+    const expected = [
+      'The Cheesecake Factory',
+      'Sudachi',
+      'Naan \'N\' Curry',
+      'Thai Stick Restaurant',
+      'Sabella & La Torre',
+      'Marrakech Moroccan Restaurant'
+    ]
+
+    assert.equal(openRestaurants.length, expected.length)
+
+    openRestaurants.forEach(({ name }) => {
+      assert(expected.includes(name))
+    })
+  })
+
+  it('Returns correct restaurants for Sunday at 12:45 am', function () {
+    const openRestaurants = getOpenRestaurants(7, '12:45 am')
+
+    const expected = [
+      'Sudachi',
+      'Naan \'N\' Curry',
+      'Thai Stick Restaurant',
+      'Marrakech Moroccan Restaurant'
+    ]
+
+    assert.equal(openRestaurants.length, expected.length)
+
+    openRestaurants.forEach(({ name }) => {
+      assert(expected.includes(name))
+    })
+  })
+
+  it('Returns correct restaurants for Sunday at 1:15 am', function () {
+    const openRestaurants = getOpenRestaurants(7, '01:15 am')
+
+    const expected = [
+      'Sudachi',
+      'Naan \'N\' Curry',
+      'Marrakech Moroccan Restaurant'
+    ]
+
+    assert.equal(openRestaurants.length, expected.length)
+
+    openRestaurants.forEach(({ name }) => {
+      assert(expected.includes(name))
+    })
+  })
+
+  it('Returns correct restaurants for Sunday at 1:30 am', function () {
+    const openRestaurants = getOpenRestaurants(7, '01:30 am')
+
+    const expected = [
+      'Naan \'N\' Curry',
+      'Marrakech Moroccan Restaurant'
+    ]
+
+    assert.equal(openRestaurants.length, expected.length)
+
+    openRestaurants.forEach(({ name }) => {
+      assert(expected.includes(name))
+    })
+  })
+
+  it('Returns correct restaurants for Monday at 12:15 am', function () {
+    const openRestaurants = getOpenRestaurants(1, '12:15 am')
+
+    const expected = [
+      'Naan \'N\' Curry',
+      'Thai Stick Restaurant',
+      'Marrakech Moroccan Restaurant'
+    ]
+
+    assert.equal(openRestaurants.length, expected.length)
+
+    openRestaurants.forEach(({ name }) => {
+      assert(expected.includes(name))
+    })
+  })
+})
